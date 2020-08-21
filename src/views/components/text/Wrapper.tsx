de7b05952b4c9ac29b4e8696208cd96d99e45102
Robin Grass <hej@carbin.se>
2020-08-22 00:34:11 +0200

    feat: added scrollbars

--- src/views/components/text/Wrapper.tsx
+++ src/views/components/text/Wrapper.tsx
@@ -2,11 +2,34 @@ import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-	width: 80vw;
-	max-width: 72rem;
+	overflow: auto;
+
+	::-webkit-scrollbar {
+		width: 6px;
+	}
+
+	::-webkit-scrollbar-track {
+		background: none;
+	}
+
+	::-webkit-scrollbar-thumb {
+		background: rgb(60, 60, 60);
+		border-radius: 10px;
+	}
+
+	::-webkit-scrollbar-thumb:hover {
+		background: rgb(180, 180, 180);
+	}
+`;
+
+const Container = styled.div`
+	padding: 2rem;
 	display: grid;
 	grid-auto-flow: row;
 	align-items: center;
+	width: 80vw;
+	max-width: 72rem;
+	max-height: 90vh;
 	gap: 2rem;
 `;
 
@@ -15,5 +38,9 @@ interface TextProps {
 }
 
 export default function Text({ children }: TextProps): JSX.Element {
-	return <Wrapper>{children}</Wrapper>;
+	return (
+		<Wrapper>
+			<Container>{children}</Container>
+		</Wrapper>
+	);
 }
